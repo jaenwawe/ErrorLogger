61ce1e3d607c91fa8c8ad3543aa3e1195fc5abde
Code Party <jnwawe@syr.edu>
2016-04-12 21:34:06 -0400

    Add database model
    Create DB Entity Framework Model

---
+++ ErrorLogESFDesgin/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using log4net;
+using System.IO;
+
+
+
+
+namespace ErrorLogESFDesgin.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public ActionResult Index()
+        {
+   
+            logger.Info("Action method on home controller called");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
